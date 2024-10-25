081fad521eca0f0985954c70d1632c530c9b1180
srdpadmin <56773212+srdpadmin@users.noreply.github.com>
2024-10-25 20:07:43 +0530

    source and publish code

---
+++ source/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    public class TestController : Controller
+    {
+        /// <summary>
+        /// Method to check if all the letters of alphabet are present in the input string
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        [HttpGet("Test/StringCheck")]
+        public bool Index(string inputString)
+        {
+            //inputString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            bool charFound = false;
+            bool result = true;
+
+            int lowerCaseAsciiIndex = 97;
+            int upperCaseAsciiIndex = 65;
+
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                for (int j = 0; j < 26; j++)
+                {
+                    charFound = false;
+                    for (int i = 0; i < inputString.Length; i++)
+                    {
+                        if ((int)inputString[i] == lowerCaseAsciiIndex ||
+                            (int)inputString[i] == upperCaseAsciiIndex)
+                        {
+                            charFound = true;
+                            break;
+                        }
+                    }
+                    if (!charFound)
+                    {
+                        result = false;
+                        break;
+                    }
+                    lowerCaseAsciiIndex++;
+                    upperCaseAsciiIndex++;
+                }
+                return result;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ source/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+namespace TestApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //Install Swashbuckle.AspNetCore
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "The API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                    
+                app.UseSwaggerUI(swag =>
+                {
+                    swag.SwaggerEndpoint("swagger/v1/swagger.json", "Test API template");
+                    swag.RoutePrefix = string.Empty;// "swagger/ui";
+                });
+            //}
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
